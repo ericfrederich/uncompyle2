8b4acf28e6e521184e517c5741faefe045837714
Eric L Frederich <eric.frederich@siemens.com>
2014-05-21 08:56:29 -0400

    work with Python 2.6

--- uncompyle2/scanner27.py
+++ uncompyle2/scanner27.py
@@ -48,7 +48,7 @@ class Scanner27(scan.Scanner):
         j = 0
         # linestarts contains bloc code adresse (addr,block)
         linestarts = list(dis.findlinestarts(co))
-        linestartoffsets = {a for (a, _) in linestarts}
+        linestartoffsets = set([a for (a, _) in linestarts])
         (prev_start_byte, prev_line_no) = linestarts[0]
         for (start_byte, line_no) in linestarts[1:]:
             while j < start_byte:
@@ -212,7 +212,7 @@ class Scanner27(scan.Scanner):
         start = 0;
         end = len(code)
         
-        stmt_opcodes = {
+        stmt_opcodes = set([
             SETUP_LOOP, BREAK_LOOP, CONTINUE_LOOP,
             SETUP_FINALLY, END_FINALLY, SETUP_EXCEPT, SETUP_WITH,
             POP_BLOCK, STORE_FAST, DELETE_FAST, STORE_DEREF,
@@ -223,15 +223,15 @@ class Scanner27(scan.Scanner):
             STORE_SLICE_0, STORE_SLICE_1, STORE_SLICE_2, STORE_SLICE_3,
             DELETE_SLICE_0, DELETE_SLICE_1, DELETE_SLICE_2, DELETE_SLICE_3,
             JUMP_ABSOLUTE, EXEC_STMT,
-        }
+        ])
 
         stmt_opcode_seqs = [(PJIF, JF), (PJIF, JA), (PJIT, JF), (PJIT, JA)]
         
-        designator_ops = {
+        designator_ops = set([
             STORE_FAST, STORE_NAME, STORE_GLOBAL, STORE_DEREF, STORE_ATTR, 
             STORE_SLICE_0, STORE_SLICE_1, STORE_SLICE_2, STORE_SLICE_3,
             STORE_SUBSCR, UNPACK_SEQUENCE, JA
-        }
+        ])
 
         prelim = self.all_instr(start, end, stmt_opcodes)
 
